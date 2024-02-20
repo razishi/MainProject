705e63ca62ef65173543438a8ecd5503801c1899
razishi <58689757+razishi@users.noreply.github.com>
2024-02-20 17:01:14 +0200

    Add files via upload

---
+++ working on/oop1_ex03-/oop1_ex03-/include/Mouse.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include"Location.h"
+
+
+
+class Mouse
+{
+public:
+	Mouse();
+	void setLocation(const int row, const int col, const char c);
+	Location getLoc();
+	char getChar();
+	void setKeyCounter(const int counterkey);
+	int getKeyCounter();
+	void setPosition(int row, int col);
+	Location getPosition();
+	Location calculateNewPosition(const Location& currentPos, char direction) const;
+	Location getInitLocation() const;
+
+
+private:
+
+	int m_row;
+	int m_col;
+	char m_char;
+	int m_key = 0;
+	Location position;
+	Location m_location;
+	Location m_initLocation;
+};
