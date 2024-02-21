bccc2cd463281388823c8921dd2cfc7fdd577b01
razishi <58689757+razishi@users.noreply.github.com>
2024-02-21 14:37:22 +0200

    Delete working on/oop1_ex03-/oop1_ex03- directory

--- working on/oop1_ex03-/oop1_ex03-/out/build/x64-Debug/ZipBackup.cmake
+++
@@ -1,5 +0,0 @@
-string (TIMESTAMP MY_TIMESTAMP "%Y-%m-%d-%H-%M-%S")
-execute_process (COMMAND C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe --build "C:/Users/shibl/Desktop/working on/oop1_ex03-/oop1_ex03-/out/build/x64-Debug" --target package_source)
-execute_process (COMMAND C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe -E copy "C:/Users/shibl/Desktop/working on/oop1_ex03-/oop1_ex03-/out/build/x64-Debug/oop1_ex03-.zip" "C:/Users/shibl/Desktop/working on/oop1_ex03-/oop1_ex03--${MY_TIMESTAMP}.zip")
-execute_process (COMMAND C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe -E echo "")
-execute_process (COMMAND C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe -E echo "***" C:/Users/shibl/Desktop/working on/oop1_ex03-/oop1_ex03--${MY_TIMESTAMP}.zip "file created ***")
